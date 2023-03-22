3d82a28f159dd0dac7ad22df656fc5f8fcee2258
William (Bily) Brown <wdbrown03@gmail.com>
2023-03-22 10:21:54 -0400

    trying to recreate what I lost yesterday. Error in stac_practice.py that is eluding me

--- stac_practice.py
+++ stac_practice.py
@@ -1,72 +1,43 @@
-import dask.distributed
-import folium
-import folium.plugins
-import geopandas as gpd
-import shapely.geometry
-# from IPython.display import HTML, display
 from pystac_client import Client
+from pystac import ItemCollection
+from odc.stac import stac_load
+from pyproj import CRS
+import logging
+import sys
+from xarray import Dataset
+from pyproj import Proj
+import rioxarray
+import geopandas as gpd
 
-from odc.stac import configure_rio, stac_load
+# Bring in geometry I want to analyze
+toms_run = gpd.read_file("TomsRun.geojson")
+toms_bbox = toms_run.total_bounds
 
+# The product and product location I want to search through
+api_url = "https://earth-search.aws.element84.com/v0"
+collection = "sentinel-s2-l2a-cogs"
 
-def convert_bounds(bbox, invert_y=False):
-    """
-    Helper method for changing bounding box representation to leaflet notation
-
-    ``(lon1, lat1, lon2, lat2) -> ((lat1, lon1), (lat2, lon2))``
-    """
-    x1, y1, x2, y2 = bbox
-    if invert_y:
-        y1, y2 = y2, y1
-    return ((y1, x1), (y2, x2))
-
-cfg = {
-    "sentinel-s2-l2a-cogs": {
-        "assets": {
-            "*": {"data_type": "uint16", "nodata": 0},
-            "SCL": {"data_type": "uint8", "nodata": 0},
-            "visual": {"data_type": "uint8", "nodata": 0},
-        },
-        "aliases": {"red": "B04", "green": "B03", "blue": "B02", "nir": "B08"},
-    },
-    "*": {"warnings": "ignore"},
-}
-
-km2deg = 1.0 / 111
-x, y = (113.887, -25.843)  # Center point of a query
-r = 100 * km2deg
-bbox = (x - r, y - r, x + r, y + r)
-
-
-catalog = Client.open("https://earth-search.aws.element84.com/v0")
-
-query = catalog.search(
-    collections=["sentinel-s2-l2a-cogs"], datetime="2021-09-16", limit=100, bbox=bbox
+# Opening and searching through the satellite imagery
+client = Client.open(api_url)
+search = client.search(
+    collections=[collection],
+    bbox=toms_bbox,
+    datetime=['2023-01-01', '2023-03-20'],
+    max_items=10,
+    query={"eo:cloud_cover": {"lt": 20, "gte":0}}
 )
 
-items = list(query.get_items())
-print(f"Found: {len(items):d} datasets")
+# Collecting the items that match the search criteria
+items = search.get_all_items()
 
-# Convert STAC items into a GeoJSON FeatureCollection
-stac_json = query.get_all_items_as_dict()
+output_crs = CRS.from_epsg(items[0].properties["proj:epsg"])
 
-gdf = gpd.GeoDataFrame.from_features(stac_json, "epsg:4326")
-
-# Compute granule id from components
-gdf["granule"] = (
-    gdf["sentinel:utm_zone"].apply(lambda x: f"{x:02d}")
-    + gdf["sentinel:latitude_band"]
-    + gdf["sentinel:grid_square"]
+dc = stac_load(
+    items=items.items,
+    bands=["B02", "B03", "B04"],
+    resolution=10,
+    crs=output_crs,
+    bbox=toms_bbox
 )
 
-fig = gdf.plot(
-    "granule",
-    edgecolor="black",
-    categorical=True,
-    aspect="equal",
-    alpha=0.5,
-    figsize=(6, 12),
-    legend=True,
-    legend_kwds={"loc": "upper left", "frameon": False, "ncol": 1},
-)
-_ = fig.set_title("STAC Query Results")
+print(dc)
